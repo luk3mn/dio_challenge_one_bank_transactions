bd64db1eb088805beb2dd7291b386bacc09d2fc3
luk3mn <lucasrenan2015@gmail.com>
2023-07-04 12:41:20 -0300

    fix: fix withdraw function and add balance calc

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ menu = """
 
 => """
 
-balance: float = 0
+balance: float = 300
 limit: float = 500
 extract: str = ""
+withdraw: float = 0
 withdraw_quantity: int = 0
 WITHDRAW_LIMIT = 3
 
 deposits = []
+withdraws = []
 while True:
 
     opcao = input(menu)
@@ -23,23 +25,24 @@ while True:
             balance += deposit
             deposits.append(deposit)
     elif opcao == "w" or opcao == "W":
-        temp_balance: float = 0
+        temp_withdraw: float = 0
         temp_withdraw_quantity: int = 0
         withdraw_value = float(input("Set your withdraw value: "))
-        if withdraw_value <= limit and withdraw_quantity <= WITHDRAW_LIMIT:
-            temp_balance = balance + withdraw_value # save a balance temporary
-            temp_withdraw_quantity = withdraw_quantity + 1 # create a count temporary
+        if withdraw_value <= limit:
+            if withdraw_value <= balance:
+                temp_withdraw = withdraw + withdraw_value # save a balance temporary
+                temp_withdraw_quantity = withdraw_quantity + 1 # create a count temporary
 
-            if temp_balance <= limit and temp_withdraw_quantity <= WITHDRAW_LIMIT: # test limit balace and withdraw quantity
-                balance += withdraw_value # count balance
-                withdraw_quantity += 1 # count withdraw
-                print(f"${withdraw_value} has been withdraw")
-                print(f"""
-                        Balance: {balance}
-                        Withdraw limit: {withdraw_quantity}
-                    """)
+                if temp_withdraw <= limit and temp_withdraw_quantity <= WITHDRAW_LIMIT: # test limit balace and withdraw quantity
+                    withdraw += withdraw_value # count balance
+                    withdraw_quantity += 1 # count withdraw
+                    balance -= withdraw_value
+                    withdraws.append(withdraw_value)
+                    print(f"${withdraw_value} has been withdraw")
+                else:
+                    print(f"The withdraw limit of R$500 has been reached or {WITHDRAW_LIMIT} withdraw has been reached")
             else:
-                print(f"The withdraw limit of R$500 has been reached or {WITHDRAW_LIMIT} withdraw has been reached")
+                print("Balance is not enough")
         else:
             print("The withdraw limit of R$500 has been reached")
     elif opcao == "e" or opcao == "E":
